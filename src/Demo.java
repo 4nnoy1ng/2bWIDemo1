8f232134e7258cf3e57d9d52fbcfbc280abaf902
4nnoy1ng <daniel.tuertscher@student.htldornbirn.at>
2022-03-16 15:53:19 +0100

    added main file

---
+++ src/Demo.java
@@ -0,0 +1,28 @@
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+
+     byte a = 7;
+     short b = 9;
+     int c = 10;
+     long d = 100;
+
+
+     double f = 10.7;
+
+
+     boolean isHuman = true;
+     String text ="dakfj ajdl fa aa";
+
+        if(c==4){
+            System.out.println("its 4");
+        } else {
+            System.out.println("its 10");
+        }
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("i:" + i);
+        }
+    }
+}
---
+++ src/IF_Bediungung_Aufgabe_2.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+public class IF_Bediungung_Aufgabe_2 {
+    public static void main(String[] args){
+        Random random1 =new Random();
+        int randomNumber1 = random1.nextInt(100);
+        System.out.println(randomNumber1);
+
+        Random random2 =new Random();
+        int randomNumber2 = random2.nextInt(100);
+        System.out.println(randomNumber2);
+
+        if ((randomNumber1<randomNumber2)&&(randomNumber1<50)) {
+            System.out.println("Zahl 1 ist kleiner als Zahl 2 und Mini");
+        }     else if ((randomNumber1 < 30)||(randomNumber2 < 30)){
+                System.out.println("Eine der beiden ist kleiner als 30");
+            } else if ((randomNumber1<randomNumber2)&&(randomNumber1<50)){
+                System.out.println("Large");
+            }
+
+        }
+    }
