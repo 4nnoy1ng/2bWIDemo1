9a93fcc0762146acfd24ed60a4ac1882f634f79c
4nnoy1ng <daniel.tuertscher@student.htldornbirn.at>
2022-05-18 15:37:23 +0200

    ArrayAufgabe3

--- src/Array3.java
+++ src/Array3.java
@@ -1,15 +1,27 @@
-import java.util.Arrays;
+public class Array3
+{
+    public static void main(String[] args)
+    {
+        int[] arr = new int[] {8, 3, 2, 22, 8, 1};
 
-public class Array3 {
-    public static void main(String[] args) {
-        int[] arr = new int[]{8, 3, 2, 22, 8, 1};
-        for (int i : arr) {
-            System.out.print(i+" ");
+        for (int i = 0; i < arr.length; i++)
+        {
+            for (int j = i + 1; j < arr.length; j++)
+            {
+                int tmp = 0;
+                if (arr[i] > arr[j])
+                {
+                    tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
         }
-        Arrays.parallelSort(arr, 0, 5);
-        System.out.println("\nAfter Sorting...");
-        for (int i : arr) {
-            System.out.print(i+" ");
+
+
+        for (int i = 0; i < arr.length; i++)
+        {
+            System.out.println(arr[i]);
         }
     }
 }
