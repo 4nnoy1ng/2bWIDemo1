47724b5551712ec4918df663984a63ce8bac110d
4nnoy1ng <daniel.tuertscher@student.htldornbirn.at>
2022-09-27 09:07:37 +0200

    TicTacToe

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,19 +1,106 @@
 import java.util.Scanner;
-
 public class TicTacToe {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int [][] playGround = new int [3][3];
-        playGround [1][0] = "X";
-        playGround [0][1] = "O";
+
+    public static void main(String [] args){
 
 
-        for (int row = 0; row < playGround.length; row++) {
-            for (int col = 0; col < playGround.length; col++) {
-                System.out.print(playGround[row][col]);
 
+        {
+
+            char[][] gameBoard = {
+                    {'_','|','_','|','_'},
+                    {'_','|','_','|','_'},
+                    {' ','|',' ','|',' '}
+            };
+
+            playerMove(gameBoard);
+
+        }
+
+
+    char[][] gameBoard = {
+                {'_','|','_','|','_'},
+                {'_','|','_','|','_'},
+                {' ','|',' ','|',' '}
+        };
+
+        printBoard(gameBoard);
+
+    }
+
+    private static void playerMove(char[][] gameBoard) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please make a move. (1-9)");
+        int move = input.nextInt();
+        updateBoard(move,1,gameBoard);
+    }
+
+    public static void printBoard(char [][] gameBoard){
+
+        for(char[] row : gameBoard){
+            for( char c : row){
+                System.out.print(c);
             }
             System.out.println();
         }
     }
+
+    public static void updateBoard(int position, int player, char[][] gameBoard) {
+
+
+
+        char character;
+
+        if (player == 1) {
+            character = 'X';
+        } else {
+            character = 'O';
+        }
+
+        switch (position) {
+
+            case 1:
+                gameBoard[0][0] = character;
+                printBoard(gameBoard);
+                break;
+            case 2:
+                gameBoard[0][2] = character;
+                printBoard(gameBoard);
+                break;
+            case 3:
+                gameBoard[0][4] = character;
+                printBoard(gameBoard);
+                break;
+            case 4:
+                gameBoard[1][0] = character;
+                printBoard(gameBoard);
+                break;
+            case 5:
+                gameBoard[1][2] = character;
+                printBoard(gameBoard);
+                break;
+            case 6:
+                gameBoard[1][4] = character;
+                printBoard(gameBoard);
+                break;
+            case 7:
+                gameBoard[2][0] = character;
+                printBoard(gameBoard);
+                break;
+            case 8:
+                gameBoard[2][2] = character;
+                printBoard(gameBoard);
+                break;
+            case 9:
+                gameBoard[2][4] = character;
+                printBoard(gameBoard);
+                break;
+
+            default:
+                break;
+
+
+        }
+    }
 }
+
---
+++ src/Wettbewerb.java
@@ -0,0 +1,6 @@
+public class Wettbewerb {
+    public static void main(String[] args) {
+     if (n>m)
+         System.out.println(0);
+    }
+}
